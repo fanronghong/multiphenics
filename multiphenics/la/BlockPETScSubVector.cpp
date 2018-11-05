13ccc5f15e12c2efb8aaf8309dc6c7d960ff0578
Francesco Ballarin <francesco.ballarin@sissa.it>
2018-11-05 12:25:26 +0100

    PETSc is now a required dependency of dolfinx

--- multiphenics/la/BlockPETScSubVector.cpp
+++ multiphenics/la/BlockPETScSubVector.cpp
@@ -16,8 +16,6 @@
 // along with multiphenics. If not, see <http://www.gnu.org/licenses/>.
 //
 
-#ifdef HAS_PETSC
-
 #include <multiphenics/la/BlockPETScSubVector.h>
 #include <multiphenics/log/log.h>
 
@@ -517,5 +515,3 @@ void BlockPETScSubVector::from_restricted_subvector_values(
   }
   dolfin_assert(restricted_value_iterator == block_restricted_subvector_values.size());
 }
-
-#endif
